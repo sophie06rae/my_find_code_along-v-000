332b87f67f3b8234671121715d42054abbe1ab26
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-16 10:04:39 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,11 +3,8 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    yield(collection[i])
+    return collection[i] if yield (collection[i])
     i += 1
   end
 end
 
-collection = [1,2,3,4]
-
-my_find(collection) {|i| i.even?}
